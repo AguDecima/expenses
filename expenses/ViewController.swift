759cef7d0bcd2133d047c98aa823d36efd4ea9ef
AguDecima <agudecima@gmail.com>
2020-04-15 20:41:19 -0300

    Initial Commit

---
+++ expenses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  expenses
+//
+//  Created by Agustin Décima on 15/04/2020.
+//  Copyright © 2020 Agustin Décima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
